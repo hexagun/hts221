e13474ca6f50f63792080a010e2083882901e607
hexagun <nv.shinde3@gmail.com>
2022-04-02 16:39:18 +0530

    code cleanup

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ int main(){
     
     unsigned char reg;
     
-    i2c_init(&bus);       
+    i2c_init(bus);       
 
     temperature = hts221_GetTemp();
     humidity = hts221_GetHumidity();
 
-    printf("Relative humidity : %.2f % \n", humidity);
+    printf("Relative humidity : %.2f \n", humidity);
     printf("Temperature in Celsius : %.2f C \n", temperature);
     printf("Temperature in Fahrenheit : %.2f F \n", (temperature*1.8) + 32);
 
     i2c_close();
-    
+
     return 0;
 }
